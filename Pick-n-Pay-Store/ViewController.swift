f6c96bb2104fca7f688307864f38aca4c6cd8fe5
PabloDeLaCruz1 <pablo.de.la.cruz.pro@gmail.com>
2022-03-30 13:10:56 -0400

    Initial Commit

---
+++ Pick-n-Pay-Store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pick-n-Pay-Store
+//
+//  Created by Pablo De La Cruz on 3/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
