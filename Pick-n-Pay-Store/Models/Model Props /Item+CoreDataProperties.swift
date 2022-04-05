ba819a22d04be62ff48a1d542631c5d0015288dc
PabloDeLaCruz1 <pablo.de.la.cruz.pro@gmail.com>
2022-04-05 12:58:20 -0400

    all entities set up, no more migrations needed for mvp

---
+++ Pick-n-Pay-Store/Models/Model Props /Item+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  Item+CoreDataProperties.swift
+//  Pick-n-Pay-Store
+//
+//  Created by Pablo De La Cruz on 4/5/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Item {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Item> {
+        return NSFetchRequest<Item>(entityName: "Item")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var price: Double
+    @NSManaged public var tags: NSObject?
+    @NSManaged public var offer: Bool
+    @NSManaged public var comments: NSObject?
+
+}
+
+extension Item : Identifiable {
+
+}
---
+++ User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  Pick-n-Pay-Store
+//
+//  Created by Pablo De La Cruz on 4/5/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
