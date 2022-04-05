d43f19534c8623564f35879f720cb6674b3825e5
Stephanie Marin <stephanienia05@gmail.com>
2022-04-05 11:11:04 -0400

    -table view

---
+++ Pick-n-Pay-Store/View/Controllers/WishListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WishListViewController.swift
+//  Pick-n-Pay-Store
+//
+//  Created by Stephanie Marin Velasquez on 4/5/22.
+//
+
+import UIKit
+
+class WishListViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
