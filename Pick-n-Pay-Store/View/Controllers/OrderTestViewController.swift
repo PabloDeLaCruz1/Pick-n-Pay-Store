bb1b0de2d2ec86ecfee93b303a14b05c233027cd
joe <joseph.m.bell111@gmail.com>
2022-04-07 17:19:12 -0400

    UI changes and error handlers

--- Pick-n-Pay-Store/View/Controllers/OrderTestViewController.swift
+++ Pick-n-Pay-Store/View/Controllers/OrderTestViewController.swift
@@ -12,24 +12,20 @@ class OrderTestViewController: UIViewController {
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
 
-    
     @IBOutlet weak var shippingOptionButton: UIButton!
     
-    
     @IBOutlet weak var payOptionButton: UIButton!
-    
     @IBOutlet weak var billingAddresstf: UITextField!
-    
-    
+
     @IBOutlet weak var shippingAddresstf: UITextField!
-    
-    
+   
     @IBOutlet weak var balanceLabel: UILabel!
-    
     
     @IBOutlet weak var shippingSwitch: UISwitch!
     
 
+    @IBOutlet weak var confirmButton: UIButton!
+    
     @IBAction func shippingSwitchAction(_ sender: Any) {
         if shippingSwitch.isOn{
             billingAddresstf.isUserInteractionEnabled = false
@@ -49,50 +45,60 @@ class OrderTestViewController: UIViewController {
             
         }
     }
-    
-    
-    @IBAction func confirmOrderButton(_ sender: Any) {
-        let cart = Cart(context: context)
-        let user = User(context: context)
-        let order = Order(context: context)
+    @IBAction func confirmOrderButton(_ sender: UIButton) {
+        self.animateView(sender)
         
-        
-        
-        appDelegate.saveContext()
-        
-        if shippingSwitch.isOn == true{
-            order.billingAddress = shippingAddresstf.text!
-            order.shippingAddress = shippingAddresstf.text!
-            order.shippingOption = shippingOptionButton.currentTitle!
-            order.payOption = payOptionButton.currentTitle!
-            order.balance = cart.total
+        if shippingAddresstf.text! == "" || billingAddresstf.text! == "" {
+            let alertController = UIAlertController(title: "Empty input field", message:
+                    "Don't forget to fill in all input fields.", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Okay", style: .default))
+
+                self.present(alertController, animated: true, completion: nil)
         }
         else{
-            order.billingAddress = billingAddresstf.text!
-            order.shippingAddress = shippingAddresstf.text!
-            order.shippingOption = shippingOptionButton.currentTitle!
-            order.payOption = payOptionButton.currentTitle!
-            order.balance = cart.total
-        }
-        
-        
-
-        appDelegate.saveContext()
-        
-        print(order.billingAddress!, order.shippingAddress!, order.payOption!, order.shippingOption!, cart.total)
-/*
-       do {
-            let items = try context.fetch(Order.fetchRequest())
-            for each in items {
-                print(each.shippingAddress!, each.billingAddress!)
+            let cart = Cart(context: context)
+            let user = User(context: context)
+            let order = Order(context: context)
+            
+            
+            
+            appDelegate.saveContext()
+            
+            if shippingSwitch.isOn == true{
+                order.billingAddress = shippingAddresstf.text!
+                order.shippingAddress = shippingAddresstf.text!
+                order.shippingOption = shippingOptionButton.currentTitle!
+                order.payOption = payOptionButton.currentTitle!
+                order.balance = cart.total
             }
-        }
-        catch{
-            print(error)
-        }
-        */
-    }
+            else{
+                order.billingAddress = billingAddresstf.text!
+                order.shippingAddress = shippingAddresstf.text!
+                order.shippingOption = shippingOptionButton.currentTitle!
+                order.payOption = payOptionButton.currentTitle!
+                order.balance = cart.total
+            }
+            
+            
 
+            appDelegate.saveContext()
+            
+            print(order.billingAddress!, order.shippingAddress!, order.payOption!, order.shippingOption!, cart.total)
+    /*
+           do {
+                let items = try context.fetch(Order.fetchRequest())
+                for each in items {
+                    print(each.shippingAddress!, each.billingAddress!)
+                }
+            }
+            catch{
+                print(error)
+            }
+            */
+            
+        }
+        
+    }
      
     enum TypeOfShipping: String, CaseIterable{
         case Overnight="Overnight"
@@ -103,8 +109,27 @@ class OrderTestViewController: UIViewController {
         case CC = "Credit Card"
     }
     
+    fileprivate func animateView(_ viewToAnimate:UIView){
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
+            viewToAnimate.transform = CGAffineTransform(scaleX: 0.92, y: 0.92)
+        }) { (_) in
+            UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 2, options: .curveEaseIn, animations: {
+                viewToAnimate.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }, completion: nil)
+    }
+}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //constraints for confirm button
+        let constraints = [
+            confirmButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80), confirmButton.centerXAnchor.constraint(equalTo: view.centerXAnchor), confirmButton.widthAnchor.constraint(equalToConstant: 200), confirmButton.heightAnchor.constraint(equalToConstant: 60),]
+        NSLayoutConstraint.activate(constraints)
+        
+        confirmButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(confirmButton)
+        confirmButton.layer.cornerRadius = 12
         
         
         let cart = Cart(context: context)
@@ -163,3 +188,4 @@ class OrderTestViewController: UIViewController {
     */
 
 }
+
