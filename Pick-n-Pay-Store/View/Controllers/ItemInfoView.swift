bccf8b72866dda6c793c1164cb5b46e7c4f46be2
Stephanie Marin <stephanienia05@gmail.com>
2022-04-14 11:36:55 -0400

    Request add new branch and add new files for wishlist + tracking ingo

---
+++ Pick-n-Pay-Store/View/Controllers/ItemInfoView.swift
@@ -0,0 +1,60 @@
+//
+//  TrackingInfo.swift
+//  Pick-n-Pay-Store
+//
+//  Created by Stephanie Marin Velasquez on 4/8/22.
+//
+//this is for shipping item details. 
+import SwiftUI
+
+struct ItemInfoView: View {
+    var body: some View {
+        VStack{
+            Text("Your \(" (item name)") is")
+                .font(.title2)
+                
+        HStack{
+            ZStack{
+            Circle()
+                .frame(width: 60, height: 60)
+                .foregroundColor(Color.gray.opacity(0.2))
+            //cart image name
+            Image(systemName: "cart.fill")
+                .font(.largeTitle)
+                .foregroundColor(Color.blue)
+        }
+         Text("Arriving Monday")
+                .font(.title)
+                .padding(.all, 15)
+                .foregroundColor(Color.blue)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+            
+            Spacer()
+            Button(action: {
+                //cancel button tapped
+            }, label: {
+                Text("cancel")
+                    .font(.body)
+                    .padding([.all], 8)
+                    .foregroundColor(Color.white)
+                    .background(Color.red)
+                    .cornerRadius(8)
+            })
+                .shadow(radius: 10)
+            
+            Spacer()
+            
+        }.padding([.leading, .trailing], 10)
+            .padding([.top, .bottom], 10)
+        }.padding([.top], 10)
+        
+    }
+}
+
+struct TrackingInfo_Previews: PreviewProvider {
+    static var previews: some View {
+        //the view is smaller
+        ItemInfoView().previewLayout(.sizeThatFits)
+    }
+}
---
+++ Pick-n-Pay-Store/View/Views/WishList/table/ItemCollectionTableViewCell.swift
@@ -0,0 +1,59 @@
+//
+//  ItemCollectionTableViewCell.swift
+//  Pick-n-Pay-Store
+//
+//  Created by Stephanie Marin Velasquez on 4/6/22.
+//
+
+// ----- this is now for wishlist content display since Pablo has a new display with SwiftUI  ---------
+
+import UIKit
+
+class ItemCollectionTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    static let identifier = "ItemCollectionTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "ItemCollectionTableViewCell", bundle: nil)
+    }
+    
+    func configure(with storeIrems: [StoreItems]){
+        self.storeItems = storeIrems
+        collectionView.reloadData()
+    }
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    //store items
+    var storeItems = [StoreItems]()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        collectionView.register(ItemCollectionViewCell.nib(), forCellWithReuseIdentifier: ItemCollectionViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    //collection view
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return storeItems.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ItemCollectionViewCell.identifier, for: indexPath) as! ItemCollectionViewCell
+        cell.configure(with: storeItems[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 250, height: 250)
+    }
+    
+    
+}
