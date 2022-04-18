b909273c52f4f946b649287b03b37711c0e95405
PabloDeLaCruz1 <pablo.de.la.cruz.pro@gmail.com>
2022-04-18 17:55:59 -0400

    SearchView draft

---
+++ Pick-n-Pay-Store/View/Views/Home/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  Pick-n-Pay-Store
+//
+//  Created by Pablo De La Cruz on 4/18/22.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
